bcf18d1ae403ce01e1a1f61a3f38146228bbb2aa
damanM <daman.m@outlook.com>
2022-12-04 13:17:48 -0500

    test net

--- src/hooks/useFireStore.js
+++ src/hooks/useFireStore.js
@@ -5,14 +5,15 @@ import { collection, onSnapshot } from "firebase/firestore";
 // we are trying to get snapshots of the data inside our firestore database that we can then use to display images to the page. 
 // first we create a hook - a func that takes the images collection as a argument 
 
- const useFireStore = (collect) => {
+const useFireStore = (collect) => {
+    
     const [docs, setDocs] = useState([])
-
-
+    
     
     useEffect(() => {
+        
         const colref = collection(projectFireStore, 'images')
-
+        
        const unsub = onSnapshot(colref, (snapshot) => {
             let images = [];
             snapshot.docs.forEach((doc) => {
@@ -20,12 +21,11 @@ import { collection, onSnapshot } from "firebase/firestore";
             })
 
             setDocs(images);
-            console.log(docs)
-
+            console.log(images)
         })
 
         return () => unsub();
-    }, [collect])
+    }, [])
     
 
     return   { docs }
