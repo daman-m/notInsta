75b0dce2ed6d883f57aacbdbb8b1213de7fc41dc
damanM <daman.m@outlook.com>
2023-03-08 13:55:39 -0500

    created a better looking landing page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,31 +1,66 @@
 import Title from "../comps/Title";
-import { Box, Heading , Link } from "@chakra-ui/react";
 import { Link as routerLink } from "react-router-dom";
 import { REGISTER } from "./routes";
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+
 
 const Home = () => {
     
     return (
-        <Box>
-            <Title />
-            <Box
-            px="2%">
-                <Heading as="h2" size="md" align="center"
-                mt="3" color="blackAlpha.800">
-            
-                 Share and interact with posts
-                </Heading>
-                <Heading size="sm" align="center" color="blackAlpha.800">
-                <Link
-                as={routerLink}
-                to={REGISTER}
-                color="blue.200"
-                >Sign up</Link> and spread the love :) </Heading>
-                
-            {/* <ImageList />  */}
-            </Box>
-        </Box>
-    )
+    <>
+    <Title/>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Welcome to social media <br />
+            <Text as={'span'} color={'blue.200'}>
+              made simple
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+          Join a community of like-minded individuals and learn how to cultivate meaningful relationships through social media, without the need for flashy tactics or chasing followers
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              colorScheme={'blue'}
+              as={routerLink}
+              to={REGISTER}
+              bg={'blue.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'blue.500',
+              }}>
+              Get Started
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 }
+
+
+    
+
 
 export default Home;
